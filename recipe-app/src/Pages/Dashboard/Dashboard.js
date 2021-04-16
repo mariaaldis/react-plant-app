92092c28fbfbbfe8759972b1dc10e21b0077449b
mariaaldis <maria.aldisg@gmail.com>
2021-04-16 10:16:43 +0300

    Http GET request added to display dashboard recipes

--- recipe-app/src/Pages/Dashboard/Dashboard.js
+++ recipe-app/src/Pages/Dashboard/Dashboard.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './Dashboard.scss';
 
 const Dashboard = () => {
+
+    const [recipes, setRecipes] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        axios.get('https://recipe-app-341cf.firebaseio.com/recipes.json')
+        .then(response => {
+            setRecipes(response.data);
+            
+        })
+        .then(() => {
+            setLoading(false);
+        });
+    },[]);
+
     return (
         <div>
-            <h1>Dashboard Works!</h1>
+            <h1>Dashboard</h1>
+            
+            <div>
+                <div>
+                    <h3>Main Meal</h3>
+                    <h5>See More</h5>
+                </div>
+                {!loading ? Object.keys(recipes).map((key, i) => {
+                    return <div className="card-container" key={key}>
+                            <img src={recipes[key].imageUrl}/>
+                            <h3>{recipes[key].recipeName}</h3>
+                        </div>
+                }) : null}
+            </div>
+            
         </div>
     );
 };
