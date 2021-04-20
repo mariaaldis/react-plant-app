8dc461bf78c3269163b5ed94198089fcee4c5dd4
mariaaldis <maria.aldisg@gmail.com>
2021-04-20 18:12:43 +0300

    Add a link to the recipe from the card component

--- recipe-app/src/Common/Card/Card.js
+++ recipe-app/src/Common/Card/Card.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import './Card.scss';
 
 const Card = (props) => {
     return (
-        <div className="card-container" key={props.key}>
-            <img src={props.imageUrl}/>
-            <h3>{props.recipeName}</h3>
-        </div>
+       
+            <div className="card-container"> 
+                <Link to={`recipe/${props.id}`}>
+                    <img src={props.imageUrl}/>
+                    <h3>{props.recipeName}</h3>
+                </Link> 
+            </div>
+        
     );
 };
 
