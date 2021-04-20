f9a5e56e6cb5a179fc1a0c1c0f5c7cfc06f3d4d1
mariaaldis <maria.aldisg@gmail.com>
2021-04-20 18:12:07 +0300

    Small fixes + pass the recipe-id to Card component

--- recipe-app/src/Pages/Dashboard/Dashboard.js
+++ recipe-app/src/Pages/Dashboard/Dashboard.js
@@ -15,44 +15,44 @@ const Dashboard = () => {
 
     useEffect(() => {
         axios.get('https://recipe-app-341cf.firebaseio.com/recipes.json?orderBy="mealType"&equalTo="Main Meal"&limitToLast=4')
-        .then(response => {
-            let recipes = response.data;
-            let newArray = [...meals];
-            newArray[0].meal = recipes;
-            newArray[0].loading = false;
-            setMeals(newArray);
-        })
-        .catch(err => console.log(err));
+            .then(response => {
+                let recipes = response.data;
+                let newArray = [...meals];
+                newArray[0].meal = recipes;
+                newArray[0].loading = false;
+                setMeals(newArray);
+            })
+            .catch(err => console.log(err));
 
         axios.get('https://recipe-app-341cf.firebaseio.com/recipes.json?orderBy="mealType"&equalTo="Dessert"&limitToLast=4')
-        .then(response => {
-            let recipes = response.data;
-            let newArray = [...meals];
-            newArray[1].meal = recipes;
-            newArray[1].loading = false;
-            setMeals(newArray);
-        })
-        .catch(err => console.log(err));
+            .then(response => {
+                let recipes = response.data;
+                let newArray = [...meals];
+                newArray[1].meal = recipes;
+                newArray[1].loading = false;
+                setMeals(newArray);
+            })
+            .catch(err => console.log(err));
 
         axios.get('https://recipe-app-341cf.firebaseio.com/recipes.json?orderBy="mealType"&equalTo="Snack"&limitToLast=4')
-        .then(response => {
-            let recipes = response.data;
-            let newArray = [...meals];
-            newArray[2].meal = recipes;
-            newArray[2].loading = false;
-            setMeals(newArray);
-        })
-        .catch(err => console.log(err));
+            .then(response => {
+                let recipes = response.data;
+                let newArray = [...meals];
+                newArray[2].meal = recipes;
+                newArray[2].loading = false;
+                setMeals(newArray);
+            })
+            .catch(err => console.log(err));
 
         axios.get('https://recipe-app-341cf.firebaseio.com/recipes.json?orderBy="mealType"&equalTo="Breakfast"&limitToLast=4')
-        .then(response => {
-            let recipes = response.data;
-            let newArray = [...meals];
-            newArray[3].meal = recipes;
-            newArray[3].loading = false;
-            setMeals(newArray);
-        })
-        .catch(err => console.log(err));
+            .then(response => {
+                let recipes = response.data;
+                let newArray = [...meals];
+                newArray[3].meal = recipes;
+                newArray[3].loading = false;
+                setMeals(newArray);
+            })
+            .catch(err => console.log(err));
     },[]);
 
     return (
@@ -60,9 +60,10 @@ const Dashboard = () => {
             <h1>Dashboard</h1>
             {meals.map(meal => {
                return !meal.loading ? (
-                    <DashboardSection mealType={meal.mealType}>
+                    <DashboardSection key={meal.mealType} mealType={meal.mealType}>
                         {Object.keys(meal.meal).map(key => {
-                            return <Card key={key} 
+                            return <Card key={key}
+                                        id={key}
                                         imageUrl={meal.meal[key].imageUrl} 
                                         recipeName={meal.meal[key].recipeName} />
                         }) }
