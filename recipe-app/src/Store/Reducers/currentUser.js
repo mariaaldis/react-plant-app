31f52ee067d5c4a37c981ad724ea5c74c4200f65
mariaaldis <maria.aldisg@gmail.com>
2021-04-22 18:45:29 +0300

    Redux initalized and currentUser reducer added

---
+++ recipe-app/src/Store/Reducers/currentUser.js
@@ -0,0 +1,10 @@
+const currentUserReducer = (state = null, action) => {
+    switch(action.type) {
+        case 'SET_CURRENT_USER':
+            return action.payload;
+        default: 
+            return state;
+    }
+}
+
+export default currentUserReducer;
